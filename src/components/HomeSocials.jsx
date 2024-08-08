553a42f7045ed82c93926f0d4c61d23868984a94
robel <robelwon75@gmail.com>
2024-08-08 22:55:10 +0300

    added components to the homepage

---
+++ src/components/HomeSocials.jsx
@@ -0,0 +1,20 @@
+import "../styles/HomeSocials.css"
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa";
+
+export default function HomeSocials(){
+	return (
+		<div className="home_socials">
+			<a href="https://linkedin.com" target="_blank">
+				<FaLinkedin className="social-icon" />
+			</a>
+			<a href="https://github.com" target="_blank">
+				<FaGithub className="social-icon" />
+			</a>
+			<a href="https://t.me/kingsforme" target="_blank">
+				<FaTelegram  className="social-icon" />
+			</a>
+		</div>
+		)
+}
