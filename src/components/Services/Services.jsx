13c1b80c12909bf8e1f65f87207389728a97e2ce
robel <robelwon75@gmail.com>
2024-09-12 18:52:18 +0300

    added sliders for the skills and services section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,38 +1,71 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "../../styles/Services.css"
+import Slider from 'react-slick'
+
+
 
 export default function Services(){
+	const services = [
+			{
+				id: 1,
+				title: "Custom Web Development",
+				paragraph1: "From concept to development, I design and develop custom web applications tailored to your business needs, using cutting-edge technologies like React, Django, and more.",
+				paragraph2: "Whether you need a simple website or a complex enterprise application, I've got you covered.",
+			},
+			{
+				id: 2,
+				title: "API Development",
+				paragraph1: "I design and develop scalable, secure, and efficient APIs that integrate seamlessly with your existing infrastructure, using Django Rest Framework and other technologies.",
+				paragraph2: "From data modeling to API development, I ensure your API is robust, reliable, and easy to maintain.",
+			},
+			{
+				id: 3,
+				title: "Code Review and Optimization",
+				paragraph1: "I review your existing codebase, identify areas for improvement, and optimize it for better performance, security, and maintainability.",
+				paragraph2: "I provide actionable recommendations and implement changes to ensure your codebase is efficient and scalable.",
+			},
+			{
+				id: 4,
+				title: "Database Modeling with Django ORM",
+				paragraph1: "I design and implement efficient, scalable, and secure database models using Django's Object-Relational Mapping (ORM) system, ensuring seamless integration with your Django applications.",
+				paragraph2: "From schema design to data migration, I ensure your database is optimized for performance and reliability.",
+			},
+			{
+				id: 5,
+				title: "Development and Handover",
+				paragraph1: "I develop your web application and provide you with a fully functional codebase, ready for deployment by your team or a third-party service.",
+				paragraph2: "I ensure a smooth handover process, providing documentation and support to help you get started with your new application.",
+			}
+
+		]
+	const settings = {
+		dots: true,
+		infinite:false,
+		speed: 500,
+		slidesToShow:1,
+		slidesToScroll:1,
+		
+	};
 	return (
 		<div id="service-container">
-			<h2>Service</h2>
-			<p id="service_paragraph">lorem ipsum is here to day to fill the space for me today until the end lorem ipsum is here to day to fill the space for me today until the end</p>
+			<h2 class="intro-title">Services</h2>
+			<p class="intro-paragraph">As a full-stack web developer, I offer a range of services to help you build scalable, efficient, and secure web applications.</p>
+			
 			<div class="service-cards">
-				<div class="service-card">
-					<i class='bx bx-code'></i>
-					<div class="service-content">
-						<h3>Web Development</h3>
-						<p>lorem ipsum is here to day to fill the space for me today until the end lorem ipsum is here to day to fill the space for me today until the end lorem ipsum is here to day to fill the space for me today until the end</p>
-						<p>lorem ipsum is here to day to fill the space for me today until the end</p>
-					</div>
-				</div>
-
-				<div class="service-card">
-					<i class='bx bx-code'></i>
-					<div class="service-content">
-						<h3>App Development</h3>
-						<p>lorem ipsum is here to day to fill the space for me today until the end</p>
-						<p>lorem ipsum is here to day to fill the space for me today until the end</p>
-					</div>
-				</div>
-
-				<div class="service-card">
-					<i class='bx bx-code'></i>
-					<div class="service-content">
-						<h3>Devop Development</h3>
-						<p>lorem ipsum is here to day to fill the space for me today until the end</p>
-						<p>lorem ipsum is here to day to fill the space for me today until the end</p>
-					</div>
-				</div>
+				<Slider {...settings}>
+					{services.map((service) => (
+						<div class="service-card" key={service.id}>
+							<i class='bx bx-code'></i>
+							<div class="service-content">
+								<h3>{service.title}</h3>
+								<p>{service.paragraph1}</p>
+								<p>{service.paragraph2}</p>
+							</div>
+						</div>
+					))}
+				</Slider>
 			</div>
 		</div>
 	)
-} 
+}
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,45 +1,101 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick'
+
 import "../../styles/Skills.css"
 import Card from "./Card"
 
 export default function Skills(){
+
+	const frontend_skills = [
+		  {
+		    icon: <i className='bx bxl-react' />,
+		    title: "REACT",
+		    description: "",
+		    bars: [
+		      { status: "Proficient", name: "State Management" },
+		      { status: "Good", name: "Handling Contexts" },
+		      { status: "Excellent", name: "Axios and ReactQuery" },
+		    ],
+		  },
+		  {
+		    icon: <i className='bx bxl-javascript' />,
+		    title: "JavaScript",
+		    description: "",
+		    bars: [
+		      { status: "Good", name: "DOM Manipulation" },
+		      { status: "Fair", name: "Async Programming" },
+		      { status: "Good", name: "WebStorage and Cookies" },
+		    ],
+		  },
+		  {
+		    icon: <i className='bx bxl-css3' />,
+		    title: "CSS and Other Frameworks",
+		    description: "",
+		    bars: [
+		      { status: "Excellent", name: "CSS" },
+		      { status: "Fair", name: "Tailwind" },
+		      { status: "Good", name: "Bootstrap" },
+		    ],
+		  },
+		];
+
+	const backend_skills = [
+			{
+		    icon: <i class='bx bxl-django'></i>,
+		    title: "DJANGO",
+		    description: "",
+		    bars: [
+					{percent:80, name:"Rest APIs"},
+					{percent:75, name:"JWT Auths"},
+					{percent:85, name:"Django's ORM"},
+				]
+				
+		  },
+		  {
+		    icon: <i className='bx bxl-flask' />,
+		    title: "Flask",
+		    description: "",
+		    bars: [
+					{percent:80, name:"CRUD"},
+					{percent:50, name:"Session Auths"},
+					{percent:65, name:"SQLAlchemy ORM"},
+				],
+		  },
+		]
+
+	const settings = {
+		dots: false,
+		infinite:false,
+		speed: 500,
+		slidesToShow:1,
+		slidesToScroll:1,
+		
+	};
+
+
 	return (
 		<div className="skills">
+			<h2 className="intro-title">Skills</h2>
+			<p className="intro-paragraph">Below are some of my key skills, categorized into frontend and backend.</p>
+
 			<div className="frontend-skills">
 				<div className="skills-title">My Frontend Skills</div>
 
 				<div className="skills-container">
-					<Card
-					icon=<i class='bx bxl-react' ></i>
-					title="REACT"
-					description=""
-					bars={[
-						 {percent:80, name:"State Management"},
-						 {percent:70, name:"Handling Contexts"},
-						 {percent: 85, name:"Axios and ReactQuery"},
-						]}
-					/>
-					<Card
-					icon=<i class='bx bxl-javascript'></i>
-					title="JavaScript"
-					description=""
-					bars={[
-						 {percent:80, name:"DOM Manipulation"},
-						 {percent:75, name:"Async Programming"},
-						 {percent:70, name:"WebStorage and Cookies"}
-						]}
-					/>
-					<Card
-					icon=<i class='bx bxl-css3'></i>
-					title="CSS and Other Frameworks"
-					description=""
-					bars={[
-						 {percent:85, name:"CSS"},
-						 {percent:55, name:"Tailwind"},
-						 {percent:60, name:"Bootstrap"},
-						]}
-					/>
-
+				<Slider {...settings}>
+				  {frontend_skills.map((skill, index) => (
+				    <Card
+				      key={index}
+				      icon={skill.icon}
+				      title={skill.title}
+				      description={skill.description}
+				      bars={skill.bars}
+				    />
+				  ))}
+				</Slider>
 				</div>
+
 			</div>
 
 {/*----------------Backend Skills -------------*/}
@@ -47,26 +103,17 @@ export default function Skills(){
 				<div className="skills-title">My Backend Skills</div>
 
 				<div className="skills-container">
-					<Card
-					icon=<i class='bx bxl-django'></i>
-					title="Django"
-					description=""
-					bars={[
-						 {percent:80, name:"Rest APIs"},
-						 {percent:75, name:"JWT Auths"},
-						 {percent:85, name:"Django's ORM"},
-						]}
-					/>
-					<Card
-					icon=<i class='bx bxl-flask' ></i>
-					title="Flask"
-					description=""
-					bars={[
-						 {percent:80, name:"CRUD"},
-						 {percent:50, name:"Session Auths"},
-						 {percent:65, name:"SQLAlchemy ORM"},
-						]}
-					/>
+				<Slider {...settings}>
+				  {backend_skills.map((skill, index) => (
+				    <Card
+				      key={index}
+				      icon={skill.icon}
+				      title={skill.title}
+				      description={skill.description}
+				      bars={skill.bars}
+				    />
+				  ))}
+				</Slider>
 				</div>
 			</div>
 
